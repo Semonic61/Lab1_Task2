f7751047a9c571d4e07250e22d47cc69fcd66d1b
Semonic61 <91585111+Semonic61@users.noreply.github.com>
2023-01-19 23:38:43 +0800

    Добавьте файлы проекта.

---
+++ Lab1_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n=");
+            var n = int.Parse(Console.ReadLine());
+            int res = 0;
+            for (int i = 1; i <= 2 * n - 1; i += 2)
+            {
+                res += i;
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
